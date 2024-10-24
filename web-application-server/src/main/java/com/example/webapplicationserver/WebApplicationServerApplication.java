0b9bb1abba708a567eb1e7f2b358cc9886305a44
givemethatsewon <nvpz1598@cau.ac.kr>
2024-10-24 20:57:28 +0900

    * chore : initialize project with Spring Boot - java 21 version

---
+++ web-application-server/src/main/java/com/example/webapplicationserver/WebApplicationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.webapplicationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicationServerApplication.class, args);
+	}
+
+}
---
+++ web-application-server/src/test/java/com/example/webapplicationserver/WebApplicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webapplicationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
