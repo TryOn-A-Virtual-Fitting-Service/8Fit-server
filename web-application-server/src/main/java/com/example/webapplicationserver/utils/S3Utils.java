50985f797f860e495a94f951bc58ba0e8b511574
givemethatsewon <nvpz1598@cau.ac.kr>
2024-10-29 21:42:10 +0900

    feat: add S3 configuration and register S3Client bean
    - Added S3 configuration for AWS access.
    - Registered S3Client bean with required credentials and region.

---
+++ web-application-server/src/main/java/com/example/webapplicationserver/utils/S3Utils.java
@@ -0,0 +1,50 @@
+package com.example.webapplicationserver.utils;
+
+import com.example.webapplicationserver.apiPayload.code.status.ErrorStatus;
+import com.example.webapplicationserver.apiPayload.exception.handler.S3ExceptionHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class S3Utils {
+    private final S3Client s3Client;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    public String uploadImage(MultipartFile image) {
+        // set unique file name
+        String imageName = UUID.randomUUID() + "_" + image.getOriginalFilename();
+
+        // object to upload to S3
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(imageName)
+                .contentType(image.getContentType())
+                .build();
+
+        // upload
+        try {
+            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(image.getBytes()));
+        } catch (IOException e) {
+            throw new S3ExceptionHandler(ErrorStatus.FILE_UPLOAD_ERROR);
+        }
+        log.info("File uploaded to S3: {}", imageName);
+
+        return "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + imageName;
+    }
+}
