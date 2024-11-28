7915633f28f4e23cdb7d85c404f5b9bc7c53cf59
givemethatsewon <nvpz1598@cau.ac.kr>
2024-11-28 21:20:49 +0900

    * fix : add request/response logging filter to basic webclient

--- web-application-server/src/main/java/com/example/webapplicationserver/config/WebClientConfig.java
+++ web-application-server/src/main/java/com/example/webapplicationserver/config/WebClientConfig.java
@@ -1,15 +1,35 @@
 package com.example.webapplicationserver.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+@Slf4j
 @Configuration
 public class WebClientConfig {
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024)) // 10MB
+                .filter(logRequest())  // 요청 로깅 필터 추가
+                .filter(logResponse()) // 응답 로깅 필터 추가
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024)) // 10MB 설정 유지
                 .build();
+    }
+
+    private ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(request -> {
+            log.info("Request: {} {}", request.method(), request.url());
+            return Mono.just(request);
+        });
+    }
+
+    private ExchangeFilterFunction logResponse() {
+        return ExchangeFilterFunction.ofResponseProcessor(response -> {
+            log.info("Response status: {}", response.statusCode());
+            return Mono.just(response);
+        });
     }
 }
